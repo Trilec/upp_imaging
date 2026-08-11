06f0aef1f4dc9c5a96d5865bcea11751c94c943a
Curtis <Trilec@users.noreply.github.com>
2026-08-11 20:53:42 +1200

    Add Imaging umbrella package

---
+++ Imaging/Imaging.h
@@ -0,0 +1,10 @@
+#ifndef _Imaging_Imaging_h_
+#define _Imaging_Imaging_h_
+
+#include <ImagingCore/ImagingCore.h>
+#include <ImagingIO/ImagingIO.h>
+#include <ImagingColor/ImagingColor.h>
+#include <ImagingAnalysis/ImagingAnalysis.h>
+#include <ImagingDiagnostics/ImagingDiagnostics.h>
+
+#endif
