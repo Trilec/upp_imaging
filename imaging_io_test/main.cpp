216b74acd089cb69f476dacfed437719367985e8
Curtis <Trilec@users.noreply.github.com>
2026-09-01 00:29:50 +1200

    Own ImagingIO shutdown before U++ app exit

--- imaging_io_test/main.cpp
+++ imaging_io_test/main.cpp
@@ -121,6 +121,7 @@ static void CheckRoundTrip(State& state, const String& root,
 
 CONSOLE_APP_MAIN
 {
+	ImageIOApplicationScope io_scope;
 	State state;
 	String root = AppendFileName(GetTempPath(), "imaging_io_contract");
 	std::filesystem::path root_path(root.Begin());
